9f27c65dee6e76d38fb632a7803e918083d48eb8
Gonçalo Vala Elias Monteiro da Cruz <13887@enautica.pt>
2023-10-03 10:35:00 +0100

    Initial commit

---
+++ src/Ex3.java
@@ -0,0 +1,17 @@
+class EX3 {
+
+    public static void main(String[] args) {
+        int x = 1;
+
+        while (x<=100){
+
+            if( x%5==0){
+                System.out.println(x);
+            }
+            x++;
+        }
+
+    }
+
+
+}
